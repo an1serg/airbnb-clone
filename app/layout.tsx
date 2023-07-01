42460429e649b577cb2422e230e0f39fa5a24665
an1serg <anissereg@gmail.com>
2023-07-01 22:57:01 +0300

    feat: add auth ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
-import Navbar from "./components/navbar/Navbar";
-import "./globals.css";
-import { Nunito } from "next/font/google";
+import RegisterModal from './components/modals/RegisterModal';
+import Navbar from './components/navbar/Navbar';
+import './globals.css';
+import { Nunito } from 'next/font/google';
+import ToasterProvider from './providers/ToasterProvider';
 
-const font = Nunito({ subsets: ["latin"] });
+const font = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Airbnb",
-  description: "Airbnb clone",
+  title: 'Airbnb',
+  description: 'Airbnb clone',
 };
 
 export default function RootLayout({
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
