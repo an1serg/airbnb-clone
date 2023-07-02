c415c6ab875cbdbfbcc1e9f7c04562dceabfd1ff
an1serg <anissereg@gmail.com>
2023-07-02 15:22:05 +0300

    feat: add socials login

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,14 @@
+import { User } from '@prisma/client';
 import Containter from '../Containter';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div
@@ -16,7 +21,7 @@ const Navbar = () => {
           <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Containter>
       </div>
