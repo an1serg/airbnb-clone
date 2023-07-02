bb47f2587bb2e18d851b221702ce43e17c06f56f
an1serg <anissereg@gmail.com>
2023-07-02 19:15:34 +0300

    feat: add categories ui

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Containter from '../Containter';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
+import Categories from './Categories';
 
 interface NavbarProps {
   currentUser?: User | null;
@@ -25,6 +26,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Containter>
       </div>
+      <Categories />
     </div>
   );
 };
